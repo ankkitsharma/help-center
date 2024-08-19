4ef2e47357659f291e4955dc5514896eef50ce41
ankkitsharma <ankitsharma2801@gmail.com>
2024-08-19 08:33:09 +0530

    connect supabase db

--- server/utils/dbConfig.ts
+++ server/utils/dbConfig.ts
@@ -4,12 +4,21 @@ import { config } from "dotenv";
 config();
 
 // Initialize pg pool middleware for working with psql db
+// const pool = new Pool({
+//   user: process.env.PG_USER,
+//   host: process.env.PG_HOST,
+//   database: process.env.PG_DATABASE,
+//   password: process.env.PG_PASSWORD,
+//   port: Number(process.env.PG_PORT),
+// });
+
+const connectionString = process.env.SUPABASE_DB_URL;
+// const pool = postgres(connectionString);
 const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: Number(process.env.PG_PORT),
+  connectionString: connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
